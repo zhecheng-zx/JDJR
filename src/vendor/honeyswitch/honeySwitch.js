e744fe3ee9d501fb6d46139fa2806d81c12a97e7
feiFly-fei <lifei3599@126.com>
2017-07-27 14:07:33 +0800

    1

--- src/vendor/honeyswitch/honeySwitch.js
+++ src/vendor/honeyswitch/honeySwitch.js
@@ -109,6 +109,88 @@ honeySwitch.showOff = function(ele) {
 		'background-color' : 'rgb(255, 255, 255)'
 	});
 }
+honeySwitch.initCertainOne = function($source) {
+    var s = "<span class='slider'></span>";
+    $source.append(s);
+    $source.click(function() {
+        if ($(this).hasClass("switch-disabled")) {
+            return;
+        }
+        if ($(this).hasClass("switch-on")) {
+            $(this).removeClass("switch-on").addClass("switch-off");
+            $(".switch-off").css({
+                'border-color' : '#dfdfdf',
+                'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+                'background-color' : 'rgb(255, 255, 255)'
+            });
+        } else {
+            $(this).removeClass("switch-off").addClass("switch-on");
+            if (honeySwitch.themeColor) {
+                var c = honeySwitch.themeColor;
+                $(this).css({
+                    'border-color' : c,
+                    'box-shadow' : c + ' 0px 0px 0px 16px inset',
+                    'background-color' : c
+                });
+            }
+            if ($(this).attr('themeColor')) {
+                var c2 = $(this).attr('themeColor');
+                $(this).css({
+                    'border-color' : c2,
+                    'box-shadow' : c2 + ' 0px 0px 0px 16px inset',
+                    'background-color' : c2
+                });
+            }
+        }
+    });
+    window.switchEvent = function(ele, on, off) {
+        $(ele).click(function() {
+            if ($(this).hasClass("switch-disabled")) {
+                return;
+            }
+            if ($(this).hasClass('switch-on')) {
+                if ( typeof on == 'function') {
+                    on();
+                }
+            } else {
+                if ( typeof off == 'function') {
+                    off();
+                }
+            }
+        });
+    }
+    if (this.themeColor) {
+        var c = this.themeColor;
+        $(".switch-on").css({
+            'border-color' : c,
+            'box-shadow' : c + ' 0px 0px 0px 16px inset',
+            'background-color' : c
+        });
+        $(".switch-off").css({
+            'border-color' : '#dfdfdf',
+            'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+            'background-color' : 'rgb(255, 255, 255)'
+        });
+    }
+    if ($('[themeColor]').length > 0) {
+        $('[themeColor]').each(function() {
+            var c = $(this).attr('themeColor') || honeySwitch.themeColor;
+            if ($(this).hasClass("switch-on")) {
+                $(this).css({
+                    'border-color' : c,
+                    'box-shadow' : c + ' 0px 0px 0px 16px inset',
+                    'background-color' : c
+                });
+            } else {
+                $(".switch-off").css({
+                    'border-color' : '#dfdfdf',
+                    'box-shadow' : 'rgb(223, 223, 223) 0px 0px 0px 0px inset',
+                    'background-color' : 'rgb(255, 255, 255)'
+                });
+            }
+        });
+    }
+};
 $(function() {
 	honeySwitch.init();
 }); 
